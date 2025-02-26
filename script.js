1ab5e0c0f377b2873d775ea678714bffff3581ad
29chinmay2004 <2022is_chinmayn_a@nie.ac.in>
2025-02-26 12:46:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Add scroll event listener to change navbar background color
 window.addEventListener('scroll', function() {
-    const navbar = document.getElementById('navbar');
-    if (window.scrollY > 50) {
-      navbar.classList.add('scrolled');
-    } else {
-      navbar.classList.remove('scrolled');
-    }
-  });
+  const navbar = document.getElementById('navbar');
+  if (window.scrollY > 100) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
